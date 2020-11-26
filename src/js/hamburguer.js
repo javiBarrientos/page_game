b49d50e1fb8fc0b7dbb9886925e76e60ca216e6e
javiBarrientos <javibarrientosfernandez99@gmail.com>
2020-11-26 16:17:14 +0100

    feat: Añadir js que crea hambuguer

---
+++ src/js/hamburguer.js
@@ -0,0 +1,8 @@
+function navBarFunction() {
+    var x = document.getElementById("links_navbar");
+    if (x.style.display === "block") {
+        x.style.display = "none";
+    } else {
+        x.style.display = "block";
+    }
+} 
